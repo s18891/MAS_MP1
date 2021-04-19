22b785e6b1e253057dccd8ff2262032921185a97
s18891 <s18891@pjwstk.edu.pl>
2021-04-19 10:04:34 +0200

    Initial commit

---
+++ src/mas/mp1/InstrumentMuzyczny.java
@@ -0,0 +1,83 @@
+package mas.mp1;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InstrumentMuzyczny extends ObjectPlus implements Serializable {
+    String nazwa;
+    String producent;
+    String rodzajMaterialu;
+    Date dataProdukcji;
+    private Boolean czyWymaganyPrzeglad;
+    Date dataOstatniegoPrzegladu;
+    List<String> osobyGrajace = new LinkedList<>();
+    static int IloscDniMiedzyPrzegladami = 365;
+
+
+
+    public InstrumentMuzyczny( String nazwa, String producent, String rodzajMaterialu) {
+        this.nazwa = nazwa;
+        this.producent = producent;
+        this.rodzajMaterialu = rodzajMaterialu;
+        this.dataProdukcji = null;
+        this.dataOstatniegoPrzegladu = new Date();
+        this.czyWymaganyPrzeglad = false;
+
+
+
+    }
+
+    public InstrumentMuzyczny( String nazwa, String producent, String rodzajMaterialu, Date dataProdukcji) {
+        this.nazwa = nazwa;
+        this.producent = producent;
+        this.rodzajMaterialu = rodzajMaterialu;
+        this.dataProdukcji = dataProdukcji;
+        this.dataOstatniegoPrzegladu = new Date();
+        this.czyWymaganyPrzeglad = false;
+    }
+
+
+
+
+    public void dodajOsobeGrajaca(String imieNazwisko) {this.osobyGrajace.add(imieNazwisko);}
+    public void wypiszOsobyGrajace() {System.out.println(Arrays.toString(this.osobyGrajace.toArray()));
+    }
+    public void usunOsobeGrajaca(int index) {this.osobyGrajace.remove(index);}
+
+    public void zapiszWykonaniePrzegladu(){ this.dataOstatniegoPrzegladu= new Date(); this.czyWymaganyPrzeglad=false;}
+    public void oznaczDoPrzegladu() {this.czyWymaganyPrzeglad=true;}
+    public void getCzyWymaganyPrzeglad() {
+        Date xDaysAgo = Date.from( Instant.now().minus( Duration.ofDays( IloscDniMiedzyPrzegladami ) ) );
+
+        if (czyWymaganyPrzeglad) System.out.println("Przegląd wymagany");
+        else if (dataOstatniegoPrzegladu.before(xDaysAgo)){
+            czyWymaganyPrzeglad=true;
+            System.out.println("Przegląd wymagany");
+        }
+        else System.out.println("Przegląd niewymagany");
+
+    }
+
+
+    public static void showExtent() throws Exception {
+        ObjectPlus.showExtent(InstrumentMuzyczny.class);
+    }
+    public String toString(){
+        if(dataProdukcji==null)return nazwa+" "+producent+" "+rodzajMaterialu;
+        else
+        return nazwa+" "+producent+" "+rodzajMaterialu +" "+dataProdukcji;
+    }
+
+
+
+
+
+
+}
+
---
+++ src/mas/mp1/Main.java
@@ -0,0 +1,62 @@
+package mas.mp1;
+
+import java.io.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	testExtentObjectPlus();
+    testSerial();
+    }
+
+    private static void testSerial() throws Exception {
+
+        var instrument1 = new InstrumentMuzyczny( "Perkusja", "Mapex", "Drewno - Brzoza");
+        instrument1.wypiszOsobyGrajace();
+        instrument1.dodajOsobeGrajaca("Lars Ulrich");
+        instrument1.dodajOsobeGrajaca("Cezary Konrad");
+        instrument1.wypiszOsobyGrajace();
+
+
+
+
+    }
+
+
+    private static void testExtentObjectPlus() throws Exception {
+        final String extentFilePath = "extent-objectplus.bin";   // TODO update the file name
+
+        var instrument1 = new InstrumentMuzyczny( "Perkusja", "Mapex", "Drewno - Brzoza");
+        //var instrument2 = new InstrumentMuzyczny("Skrzypce", "Stradivarius", "Drewno - Mahoń");
+
+        var instrument3 = new InstrumentMuzyczny("Skrzypce", "Stradivarius", "Drewno - Mahoń", new GregorianCalendar(2020, 12 - 1, 1).getTime());
+
+
+        InstrumentMuzyczny.showExtent();
+
+        try {
+            // Write the extent to the stream
+            var out = new ObjectOutputStream(new FileOutputStream(extentFilePath));
+            ObjectPlus.writeExtents(out);
+            out.close();
+
+            // Read the extent from the stream
+            var in = new ObjectInputStream(new FileInputStream(extentFilePath));
+            ObjectPlus.readExtents(in);
+            in.close();
+
+            // Gets the extent and show the content of it
+            Iterable<InstrumentMuzyczny> instrumentMuzycznyExtent = ObjectPlus.getExtent(InstrumentMuzyczny.class);
+            for (var instrumentMuzyczny : instrumentMuzycznyExtent) {
+                System.out.println(instrumentMuzyczny.nazwa);
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
+        InstrumentMuzyczny.showExtent();
+    }
+}
